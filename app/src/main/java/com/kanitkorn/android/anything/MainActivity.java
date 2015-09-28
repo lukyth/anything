1f817132eb14d6e68dc68767b4f37a52986c9865
Kanitkorn S <k.sujautra@gmail.com>
2015-09-28 09:51:51 +0700

    Add various AutoCompleteTextView implementation
    - Static: AutoCompleteTextView with fixed data to complete text from an array.
    - Dynamic: Add AutoCompleteTextView dynamically into an app
    - Runtime: Change data to complete text OTA

--- app/src/main/java/com/kanitkorn/android/anything/MainActivity.java
+++ app/src/main/java/com/kanitkorn/android/anything/MainActivity.java
@@ -1,18 +1,30 @@
 package com.kanitkorn.android.anything;
 
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ToggleButton toggleButton1, toggleButton2;
     private EditText editText;
+
+    List<String> listAutoCompleteRuntime;
+    AutoCompleteTextView autoCompleteTextViewRuntime;
+
+    String str[] = {"Arun", "Mathev", "Vishnu", "Vishal", "Arjun",
+            "Arul", "Balaji", "Babu", "Boopathy", "Godwin", "Nagaraj"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +32,82 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addListenerOnToggleButton();
+
         addListenerOnEditText();
+
+        initAutoCompleteStatic();
+        initAutoCompleteDynamic();
+        initAutoCompleteRuntime();
+
+    }
+
+    public void initAutoCompleteStatic() {
+        AutoCompleteTextView autoCompleteTextViewStatic = (AutoCompleteTextView)
+                findViewById(R.id.autoCompleteTextView1);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, str);
+
+        autoCompleteTextViewStatic.setThreshold(1);
+        autoCompleteTextViewStatic.setAdapter(arrayAdapter);
+    }
+
+    public void initAutoCompleteDynamic() {
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
+        AutoCompleteTextView autoCompleteTextView = new AutoCompleteTextView(MainActivity.this);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+
+        autoCompleteTextView.setLayoutParams(params);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, str);
+
+        autoCompleteTextView.setThreshold(1);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+
+        linearLayout.addView(autoCompleteTextView);
+    }
+
+    public void initAutoCompleteRuntime() {
+
+        listAutoCompleteRuntime = new ArrayList<>();
+        listAutoCompleteRuntime.add("Item 1");
+        listAutoCompleteRuntime.add("Item 2");
+        listAutoCompleteRuntime.add("Item 3");
+
+        autoCompleteTextViewRuntime = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView2);
+        final EditText editText1 = (EditText) findViewById(R.id.editText1);
+        Button button1 = (Button) findViewById(R.id.button1);
+
+        add();
+
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                listAutoCompleteRuntime.add(editText1.getText().toString());
+                editText1.setText(null);
+
+                add();
+
+                Toast.makeText(getBaseContext(), "Item Added",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void add() {
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, listAutoCompleteRuntime);
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        autoCompleteTextViewRuntime.setThreshold(1);
+        autoCompleteTextViewRuntime.setAdapter(arrayAdapter);
     }
 
     public void addListenerOnToggleButton() {
